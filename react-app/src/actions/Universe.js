aa48e9662e8d81cf8d17e0c36a75c8496e48807e
arushr <wanemphago@gmail.com>
2019-01-22 03:54:59 -0500

    Delete Universe.js

--- react-app/src/actions/Universe.js
+++
@@ -1,8 +0,0 @@
-import * as UniverseActionTypes from "../actiontypes/Universe";
-
-export const collapseA = () => {
-  return { type: UniverseActionTypes.COLLAPSE_UNIVERSE_A };
-};
-export const collapseB = () => {
-  return { type: UniverseActionTypes.COLLAPSE_UNIVERSE_B };
-};
